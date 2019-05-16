a524d9ce0b14965367a938d2a4d240b7eba0c736
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-15 19:35:03 -0600

    In stock Alert
    created the alert for if the item is in stock or out

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :department
+
+  def instock_alert
+    if self.instock == true
+      "We have some #{self.name} in stock"
+    else
+      "Sorry #{self.name} are out of stock"
+    end
+  end
 end
